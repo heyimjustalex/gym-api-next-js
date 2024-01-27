b0034a919a5797be3e1fa4001641d7b90bb8dae0
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2024-01-27 22:34:08 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,4 +2,8 @@
 set -e
 echo "Deploying to ${DEPLOYMENT_ENVIRONMENT}"
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-echo $GCP_PROJECT_KEY 
+echo $GCP_PROJECT_KEY > service_key.txt
+base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
+cat ${HOME}/gcloud-service-key.json
+
+
