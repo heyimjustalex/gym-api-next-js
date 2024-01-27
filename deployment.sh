ea48d6dff3379b292a8307a417c6f45c80c06ca6
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2024-01-27 22:30:48 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Deploying to ${DEPLOYMENT_ENVIRONMENT}"
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-echo $ACCOUNT_KEY_STAGING > service_key.txt
+echo $GCP_PROJECT_KEY > service_key.txt
 base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-461.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-461.0.0-linux-x86_64.tar.gz
