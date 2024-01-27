701a57fc7fe4235b22c9d4b36c21ad18ca69e3d0
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2024-01-27 21:48:28 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -7,3 +7,5 @@ base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-461.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-461.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
+gcloud components update
+gcloud version
