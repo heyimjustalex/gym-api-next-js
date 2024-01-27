d9d27610275aab96031c1159bfd67ebf74ed68a7
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2024-01-27 22:22:33 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -7,6 +7,6 @@ base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-461.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-461.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
-./google-cloud-sdk/bin/gcloud version
+./google-cloud-sdk/bin/gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/nextapp-key.json
 
 
