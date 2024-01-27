070782132c9fa608cf431d37947c4ef47be8f12b
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2024-01-27 21:32:45 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 set -e
 echo "Deploying to ${DEPLOYMENT_ENVIRONMENT}"
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 echo $ACCOUNT_KEY_STAGING > service_key.txt
 base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 curl https://sdk.cloud.google.com | bash
